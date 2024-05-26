0938ba056becbedc8f79d89126e8e44e32a200b6
Safiyan <safianrafi2004@gmail.com>
2024-05-26 23:44:24 +0500

    new changes

--- Simple_Inventory.cpp
+++ Simple_Inventory.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iomanip>
 #include <algorithm>
+#include <map>
 using namespace std;
 struct stock
 {
@@ -10,6 +11,13 @@ struct stock
   int item_Quantity;
   double item_Price;
 };
+struct supplier_products
+{
+    string Supplier_item_Name;
+    int Supplier_item_Quantity;
+    int Supplier_item_Price;
+};
+
 class inventory
 {
     private:
@@ -24,7 +32,7 @@ class inventory
     //-------------------------
     vector <string> Supplier_Username_Saved;
     vector <string> Supplier_Password_Saved;
-
+     
     //-------------------------
     //         ITEMS
     //-------------------------
@@ -33,6 +41,10 @@ vector <stock> product = {{"Beans","Vegetable",50,12.76},{"Bananas","Fruit",100,
                           ,{"Tomato","Vegetable",30,14},{"Strawberry","Fruit",85,17.9}
                           ,{"Shampoo","Cosmetics",90,25.1},{"Lotion","Cosmetics",30,14},
                           {"Hair Oil","Cosmetics",85,17.9}};
+    //-------------------------
+    //    SUPPLIER ITEMS
+    //-------------------------
+    vector <supplier_products> items;
 
     public:
     //-------------------------
@@ -66,14 +78,14 @@ vector <stock> product = {{"Beans","Vegetable",50,12.76},{"Bananas","Fruit",100,
             cout << "Wrong Info ! \nAccess Denied!"<<endl;
             cout << " 1) Try Again : "<<endl;
             cout << " 2) Exit : ";
-            int choice;
+            char choice;
             cin >> choice;
             switch (choice) 
             {
-                case 1:
+                case '1':
                     Admin_Authorization();
                     break;
-                case 2:
+                case '2':
                     execute();
                     break;
                 default:
@@ -89,7 +101,7 @@ vector <stock> product = {{"Beans","Vegetable",50,12.76},{"Bananas","Fruit",100,
 void Admin_Menu()
 {
     system("cls");
-    int choice;
+    char choice;
     cout << "\t\t================" << endl;
     cout << "\t\t  WELCOME BACK" << endl;
     cout << "\t\t================\n\n" << endl;
@@ -102,19 +114,19 @@ void Admin_Menu()
     cin>>choice;
     switch(choice)
     {
-        case 1:
+        case '1':
         Display_Stock();
         break;
-        case 2:
+        case '2':
         Existing_Stock_Change();
         break;
-        case 3:
+        case '3':
         Add_New_Stock();
         break;
-        case 4:
+        case '4':
         Display_Categories();
         break;
-        case 5:
+        case '5':
         execute();
         break;
         default:
@@ -148,16 +160,16 @@ void Existing_Stock_Change()
     if(stockitem < 1 || stockitem > product.size())
     {
         system("cls");
-        int Verify;
+        char Verify;
         cout << "The Number Your Entered Is Out of Range ! " << endl;
               cout << " 1) Try Again : " ;
               cout << " 2) Exit : " ;
               cin >> Verify;
-              if(Verify == 1)
+              if(Verify == '1')
               {
                   Existing_Stock_Change();
               }
-              else if(Verify == 2)
+              else if(Verify == '2')
               {
                   Admin_Menu();
               }
@@ -228,7 +240,7 @@ void Supplier_Menu()
     cout << "\t\t========================"<<endl;
     cout << "\t\t    SUPPLIER MENU "<<endl;
     cout << "\t\t========================\n"<<endl<<endl;
-    int choice;
+    char choice;
     cout << " 1) SIGN UP  "<<endl;
     cout << " 2) LOGIN "<<endl;
     cout << " 3) EXIT \n"<<endl;
@@ -236,13 +248,13 @@ void Supplier_Menu()
     cin >> choice ;
     switch (choice)
     {
-    case 1:
+    case '1':
     supplier_Signup();
     break;
-    case 2:
+    case '2':
     supplier_Authorization();
     break;
-    case 3:
+    case '3':
     execute();
     break;
     default:
@@ -253,6 +265,9 @@ void Supplier_Menu()
     break;
     }
 }
+//-------------------------
+//    SUPPLIER SIGNUP
+//-------------------------
 void supplier_Signup()
 {
     system("cls");
@@ -282,6 +297,9 @@ void supplier_Signup()
         Supplier_Menu();
     }
 }
+//---------------------------
+//   SUPPLIER AUTHORIZATION
+//---------------------------
 void supplier_Authorization()
  {
     system("cls");
@@ -300,18 +318,18 @@ void supplier_Authorization()
         system("cls");
         cout << " \n\nLOGIN DONE !! \n" <<endl<<endl;
         system("pause");
-        Supplier_Purchasing();
+        Supplier_Choice();
     }
     else 
     {
-        int choice;
+        char choice;
         system("cls");
         cout << "\n\nTHE USERNAME OR PASSPORT YOU ENTERED IS WRONG \n\n!! "<<endl;
         cout << " 1) TRY AGAIN "<<endl;
         cout << " 2) EXIT "<<endl;
         cout << " ENTER YOUR CHOICE : ";
         cin >> choice;
-        if(choice == 1)
+        if(choice == '1')
         {
            supplier_Authorization();
         }
@@ -321,11 +339,14 @@ void supplier_Authorization()
         }
     }
     }
-void Supplier_Purchasing()
+//---------------------------
+//   SUPPLIER PURCHASING 
+//---------------------------
+void Supplier_Choice()
 {
 system("cls");
      cout << "\t\t======================" << endl;
-    cout << "\t\t   BUYING INTERFACE" << endl;
+    cout << "\t\t    WARHOUSE STOCK " << endl;
     cout << "\t\t======================\n\n" << endl;
       cout<< setw(10) << left << "No" << setw(20) << left << "Item Name " 
         << setw(15) << left <<"Category" << setw(15) << left <<"Quantity"<< setw(10) << left << "Price";
@@ -337,21 +358,21 @@ system("cls");
                 << setw(15) << left << product[i].item_category << setw(15) << left << product[i].item_Quantity 
                  << fixed << setprecision(2) <<"$ "<< product[i].item_Price<<endl;
         }
-    int choice;
-    cout<<"\n\n 1) BUY PRODUCTs : "<<endl;
+    char choice;
+    cout<<"\n\n 1) BUY PRODUCTS : "<<endl;
     cout<<" 2) VIEW BOUGHT PRODUCTS : "<<endl;
     cout<<" 3) LOGOUT : "<<endl;
     cout<<"\nENTER A CHOICE : ";
     cin >> choice;
     switch(choice)
     {
-        case 1:
-         Supplier_Purchased_Products();
+        case '1':
+        Supplier_Purchasing_Products();
         break;
-        case 2:
-        Display_Categories();
+        case '2':
+        Supplier_Item_Storage();
         break;
-        case 3:
+        case '3':
         execute();
         break;
         default:
@@ -361,9 +382,90 @@ system("cls");
         Supplier_Menu();
     }
 }
-void Supplier_Purchased_Products()
-{
+void Supplier_Purchasing_Products() {
+    system("cls");
+    char choice;
+    cout << "\t\t\t======================" << endl;
+    cout << "\t\t\t  BUYING INTERFACE " << endl;
+    cout << "\t\t\t======================\n\n" << endl;
+     cout<< setw(10) << left << "No" << setw(20) << left << "Item Name " 
+        << setw(15) << left <<"Category" << setw(15) << left <<"Quantity"<< setw(10) << left << "Price";
+        cout<<endl;
+        cout<<endl;
+          for (size_t i = 0; i < product.size(); i++)
+        {
+            cout<< setw(10) << left << i +1 << setw(20) << left << product[i].item_Name
+                << setw(15) << left << product[i].item_category << setw(15) << left << product[i].item_Quantity 
+                 << fixed << setprecision(2) <<"$ "<< product[i].item_Price<<endl;
+        }
+    cout << "\n\n\nPRESS 1 TO BUY 10 ITEMS " << endl;
+    cout << "PRESS 2 TO BUY 5 ITEMS " << endl;
+    cout << "PRESS E/e TO RETURN TO TH STOCK INTERFACE \n\n" << endl;
+    cout << "ENTER YOUR CHOICE : ";
+    cin >> choice;
 
+    switch (choice) {
+        case '1':
+        case '2': 
+        {
+            char Num_To_Buy = (choice == '1') ? 10 : 5;
+            cout << "ENTER THE NUMBER OF ITEM TO BE BOUGHT : ";
+            int itemNumber;
+            cin >> itemNumber;
+            if (itemNumber < 1 || itemNumber > product.size()) 
+            {
+                cout << "INVALID ITEM NUMBER !! \n\n" << endl;
+                system("pause");
+                Supplier_Purchasing_Products();
+                return;
+            }
+            int index = itemNumber - 1;
+            if (product[index].item_Quantity >= Num_To_Buy)
+            {
+                supplier_products supplier_item;
+                supplier_item.Supplier_item_Name = product[index].item_Name;
+                supplier_item.Supplier_item_Quantity = product[index].item_Quantity;
+                supplier_item.Supplier_item_Price = product[index].item_Price;
+                items.push_back(supplier_item);
+                product[index].item_Quantity -= Num_To_Buy;
+                cout << "\n\nPURCHASE SUCCESFUL \n!! " << endl;
+            } 
+            else 
+            {
+                cout << "\n\nNOT ENOUGH QUANTITY AVAILABLE\n !! " << endl;
+            }
+            break;
+        }
+        case 'E':
+        case 'e':
+            Supplier_Choice();
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+    }
+    system("pause");
+    Supplier_Purchasing_Products();
+}
+void Supplier_Item_Storage()
+{
+    system("cls");
+    cout<<"\t\t========================" << endl;
+    cout<<"\t\t   SUPPLIER STORAGE " << endl;
+    cout<<"\t\t========================" << endl;
+    cout << endl;
+    cout << endl;
+    cout<< setw(10) << left << "No" << setw(20) << left << "Item Name " 
+        << setw(15) << left <<"Quantity"<< setw(10) << left << "Price";
+
+        cout<<endl;
+        cout<<endl;
+        for (size_t i = 0; i < items.size() ; i++)
+        {
+      cout<< setw(10) << left << i +1 << setw(20) << left << items[i].Supplier_item_Name
+                 << setw(15) << left << items[i].Supplier_item_Quantity 
+                 << fixed << setprecision(2) <<"$ "<< items[i].Supplier_item_Price<<endl;
+        }
+        
 }
 //---------------------------
 //     DISPLAY ITEMS 
@@ -392,111 +494,51 @@ void Display_Stock()
 //---------------------------
 // DISPLAY CATOGORIED ITEMS 
 //---------------------------
-void Display_Categories()
-{
-    system("cls");
-    cout << "\t====================" << endl;
-    cout<<  "\t     CATEGORIES"<<endl;
-    cout << "\t====================\n\n" << endl;
-    int choice;
-    cout<<" 1) FRUITS : "<<endl;
-    cout<<" 2) VEGETABLES : "<<endl;
-    cout<<" 3) COSMETICS : "<<endl;
-    cout<<" 4) EXIT : "<<endl;
-    cout<<"ENTER A CHOICE : ";
-    cin >> choice;
-    system("cls");
-    int count = 1;
-    switch(choice)
-    {
-        case 1:
-        cout << "\t\t=============" << endl;
-        cout << "\t\t   FRUITS" << endl;
-        cout << "\t\t=============\n\n" << endl;
-           cout << setw(10) << left << "No" << setw(20) << left << "Item Name "
-         << setw(15) << left << "Category" << setw(15) << left << "Quantity" << setw(10) << left << "Price";
-    cout << endl;
-    cout << endl;
-    for (size_t i = 0; i < product.size(); i++)
-    {
-        if (product[i].item_category == "fruit" ||
-            product[i].item_category == "Fruit")
+void Display_Categories() {
+        system("cls");
+        cout << "\t\t====================" << endl;
+        cout << "\t\t     CATEGORIES" << endl;
+        cout << "\t\t====================\n\n" << endl;
+
+        map<string, vector<stock>> categories;
+        for (auto item : product) 
         {
-            cout << setw(10) << left << count << setw(20) << left << product[i].item_Name
-                 << setw(15) << left << product[i].item_category << setw(15) << left << product[i].item_Quantity
-                 << fixed << setprecision(2) << "$ " << product[i].item_Price << endl;
-            count++;
+            categories[item.item_category].push_back(item);
         }
-    }
-        cout << endl;
-        cout << endl;
-        system("pause");
-        Display_Categories();
-        break;
-        case 2:
-        cout << "\t\t=============" << endl;
-        cout << "\t\t  VEGETABLES" << endl;
-        cout << "\t\t=============\n\n" << endl;
-        cout << setw(10) << left << "No" << setw(20) << left << "Item Name "
-         << setw(15) << left << "Category" << setw(15) << left << "Quantity" << setw(10) << left << "Price";
-    cout << endl;
-    cout << endl;
-    for (size_t i = 0; i < product.size(); i++)
-    {
-        if (product[i].item_category == "Vegetable"||
-            product[i].item_category == "vegetable")
+        for (auto category: categories)
         {
-             cout << setw(10) << left << count << setw(20) << left << product[i].item_Name
-                 << setw(15) << left << product[i].item_category << setw(15) << left << product[i].item_Quantity
-                 << fixed << setprecision(2)  << "$ " << product[i].item_Price << endl;
-            count++;
+            cout << "\t\t=============" << endl;
+            cout << "\t\t  " << category.first << endl;
+            cout << "\t\t=============\n\n" << endl;
+
+            cout << setw(10) << left << "No" << setw(20) << left << "Item Name "
+                 << setw(15) << left << "Quantity" << setw(10) << left << "Price";
+            cout << endl;
+            cout << endl;
+
+            int count = 1;
+            for (auto item : category.second) 
+            {
+                cout << setw(10) << left << count << setw(20) << left << item.item_Name
+                     << setw(15) << left << item.item_Quantity
+                     << fixed << setprecision(2) << "$ " << item.item_Price << endl;
+                count++;
+            }
+
+            cout << endl;
+            cout << endl;
         }
-    }
-        cout << endl;
-        cout << endl;
-        system("pause");
-        Display_Categories();
-        break;
-        case 3:
-        cout << "\t\t=============" << endl;
-        cout << "\t\t  COSMETICS" << endl;
-        cout << "\t\t=============\n\n" << endl;
-        cout << setw(10) << left << "No" << setw(20) << left << "Item Name "
-         << setw(15) << left << "Category" << setw(15) << left << "Quantity" << setw(10) << left << "Price";
-    cout << endl;
-    cout << endl;
-    for (size_t i = 0; i < product.size(); i++)
-    {
-        if (product[i].item_category == "Cosmetics"||
-            product[i].item_category == "cosmetics")
-        {
-             cout << setw(10) << left << count << setw(20) << left << product[i].item_Name
-                 << setw(15) << left << product[i].item_category << setw(15) << left << product[i].item_Quantity
-                 << fixed << setprecision(2)  << "$ " << product[i].item_Price << endl;
-            count++;
-        }
-    }
-        cout << endl;
-        cout << endl;
-        system("pause");
-        Display_Categories();
-        break;
-        case 4:
-        Admin_Menu();
-        break;
-        default:
-        cout << "INVALID CHOICE !! "<<endl;
+
         system("pause");
         Admin_Menu();
     }
-}
 //---------------------------
 // Program Execute FUNCTION
 //---------------------------
 void execute()
 {
     system("cls");
-    int choice;
+    char choice;
     cout << "\t===========================" << endl;
     cout << "\t WELCOME TO THE INVENTORY" << endl;
     cout << "\t===========================\n\n" << endl;
@@ -507,21 +549,20 @@ void execute()
     cin>>choice;
     switch(choice)
     {
-        case 1:
+        case '1':
         Admin_Authorization();
         break;
-        case 2:
+        case '2':
         Supplier_Menu();
         break;
-        case 3:
+        case '3':
         exit(0);
         break;
         default:
         system("cls");
-        cout<<"Invalid Choice !";
+        cout<<"Invalid Choice !\n\n";
         system("pause");
         execute();
-        break;
     }
 }
 };
